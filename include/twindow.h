576c22a34b2231c5c3a78720a3027228e126cc38
oseias <oseiasisidoro@gmail.com>
2024-04-13 17:48:01 -0300

    modify include/twindow.h

--- include/twindow.h
+++ include/twindow.h
@@ -5,10 +5,16 @@
 #ifndef WMTUPI_TWINDOW_H
 #define WMTUPI_TWINDOW_H
 
-#endif //WMTUPI_TWINDOW_H
+    TWindow* createTWindow(WMT* wmt, Window* win, int border_width);
 
-TWindow* createTWindow(WMT* wmt, Window* win, int border_width);
-void     resizeTWindow(WMT* wmt, TWindow* tWin, int width, int height, int border_width);
-void     moveTWindow(WMT* wmt, TWindow* tWin, int x, int y);
-void     mapTWindow(WMT* wmt, TWindow* tWin);
-void     unmapTWindow(WMT* wmt, TWindow* tWin);
+    void     resizeTWindow(WMT* wmt, TWindow* tWin, int width, int height, int border_width);
+
+    void     moveTWindow(WMT* wmt, TWindow* tWin, int x, int y);
+
+    void     mapTWindow(WMT* wmt, TWindow* tWin);
+
+    void     unmapTWindow(WMT* wmt, TWindow* tWin);
+
+    void maximizeTWindow(WMT* wmt, TWindow* tWin);
+
+#endif //WMTUPI_TWINDOW_H
