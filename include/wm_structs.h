2b7ff7615aa92adf93510b4ac57ce031557fc27b
oseias <oseiasisidoro@gmail.com>
2024-04-13 17:50:27 -0300

    modify include/wm_structs.h

--- include/wm_structs.h
+++ include/wm_structs.h
@@ -5,32 +5,34 @@
 #ifndef WMTUPI_WM_STRUCTS_H
 #define WMTUPI_WM_STRUCTS_H
 
+    typedef struct TupiWindow {
+        Window* frame;
+        Window* window;
+        int border_width;
+        int width;
+        int height;
+    } TWindow;
+
+    typedef struct {
+        int size;
+        TWindow *arr;
+    } DynamicArray;
+
+    typedef struct {
+        int width;
+        int height;
+        Screen* screen;
+        Window win_root;
+        GC gc;
+        Colormap screen_colormap;
+        XFontStruct* font_info;
+    } TScreen;
+
+    typedef struct {
+        int run;
+        Display* display;
+        TScreen* screen;
+    } WMT;
+
 #endif //WMTUPI_WM_STRUCTS_H
 
-typedef struct TupiWindow {
-    Window* frame;
-    Window* window;
-    int border_width;
-    int width;
-    int height;
-} TWindow;
-
-typedef struct {
-    int size;
-    TWindow *arr;
-} DynamicArray;
-
-typedef struct {
-    int width;
-    int height;
-    Screen* screen;
-    Window win_root;
-    TWindow * windows[5];
-    Colormap screen_colormap;
-} TScreen;
-
-typedef struct {
-    int run;
-    Display* display;
-    TScreen* screen;
-} WMT;
