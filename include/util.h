ff0540b38b8277a2023484feec0da2cfb4ffc358
oseias <oseiasisidoro@gmail.com>
2024-04-13 17:48:25 -0300

    modify include/util.h

--- include/util.h
+++ include/util.h
@@ -5,12 +5,12 @@
 #ifndef WMTUPI_UTIL_H
 #define WMTUPI_UTIL_H
 
+    TScreen* createTScreen(Display* display);
+
+    DynamicArray createDynamicArray();
+
+    void add(DynamicArray* arr, TWindow item);
+
+    void removeItem(DynamicArray* arr, int indexToRemove);
+
 #endif //WMTUPI_UTIL_H
-
-TScreen* createTScreen(Display* display);
-
-DynamicArray createDynamicArray();
-
-void add(DynamicArray* arr, TWindow item);
-
-void removeItem(DynamicArray* arr, int indexToRemove);
