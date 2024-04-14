d52091f22bde4ae0f93846c82a57dbd77f2af7b5
oseias <oseiasisidoro@gmail.com>
2024-04-14 19:37:51 -0300

    creates include/config.h

---
+++ include/config.h
@@ -0,0 +1,18 @@
+//
+// Created by Oseias-Isidoro on 13/04/24.
+//
+
+#ifndef WMTUPI_CONFIG_H
+#define WMTUPI_CONFIG_H
+
+#define MASTER_KEY            Mod4Mask
+#define TERMINAL_OPEN_KEY     XK_Return
+#define TAB                   XK_Tab
+#define MAXIMIZE_KEY          XK_M
+#define MOVE_WINDOW_LEFT_KEY  XK_Left
+#define MOVE_WINDOW_RIGHT_KEY XK_Right
+
+#define SCREEN_TOP_BAR_HEIGHT 36
+#define WIN_TOP_BAR_HEIGHT 0
+
+#endif //WMTUPI_CONFIG_H
