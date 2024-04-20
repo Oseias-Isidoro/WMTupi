0a510b5968d3f0bc68e6b77361debcc6e8bc87fb
oseias <oseiasisidoro@gmail.com>
2024-04-20 01:19:17 -0300

    add header for functions getDisplay, grab_input, allocColor include/util.h

--- include/util.h
+++ include/util.h
@@ -5,7 +5,13 @@
 #ifndef WMTUPI_UTIL_H
 #define WMTUPI_UTIL_H
 
+    Display* getDisplay();
+
     TScreen* createTScreen(Display* display);
+
+    void grab_input(WMT* wmt, Window win, unsigned int master_key, unsigned int action_key);
+
+    void allocColor(Display* display, XColor* xcolor, char* color_code);
 
     DynamicArray createDynamicArray();
 
